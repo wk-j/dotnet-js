390a13d344fb2bd4f604a98af52f920b530efdd8
wk <somnuk.wk@gmail.com>
2023-01-24 23:58:44 +0700

    Build js

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 ```bash
 sudo dotnet workload install wasm-tools
+dotnet publish src/MyApp --configuration Release
+```
+
+#  Run
+
+```bash
+dotnet tool restore
+dotnet serve --directory src/MyApp/bin/Release/net7.0/browser-wasm/AppBundle
 ```
--- src/MyApp/Program.cs
+++ src/MyApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices.JavaScript;
+
+// Create a "Main" method. This is required by the tooling.
+return;
+
+public partial class Sample {
+    // Make the method accessible from JS
+    [JSExport]
+    internal static int Add(int a, int b) {
+        return a + b;
+    }
+}
